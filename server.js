be09c57c0dcdeb378eb9c043a1fee93ece98b4bd
Hina1902 <haavatar123@gmail.com>
2025-12-10 19:13:57 +0700

    Fix env mongo uri

--- server.js
+++ server.js
@@ -31,9 +31,10 @@ app.use(cors());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log('>>>>>> MongoDB connected'))
     .catch(err => console.log('>>>>>> DB Error', err));
+
 
 
 app.use('/api/account', accountRouter);
